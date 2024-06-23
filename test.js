e1e19e1ed0ff93161952ffcd6d8d3ccb9ce017ec
Rajjak808 <mohammadrajjak808@gmail.com>
2024-06-23 10:22:18 +0600

    bank

---
+++ test.js
@@ -0,0 +1,4 @@
+const loginBtn document.getElementById(login);
+loginBtn.addEventListener("click",function(){
+const loginArea = document.getElementById("login-area");
+loginArea.style.display ="none"; });
